3c9daee3045a04bd834141074cbce4e48e41d10f
Juan Pablo Pérez Lantes <jplantes@gmail.com>
2024-03-07 14:48:55 -0300

    Paquete Español para md-editor-v3

---
+++ index.ts
@@ -0,0 +1,91 @@
+import type { StaticTextDefaultValue } from 'md-editor-v3'
+
+
+/**
+ * @author Juan Pablo Pérez Lantes
+ * @email  jplantes@gmail.com
+ * @github https://github.com/jplantes
+ *
+ * Español (Argentina)
+ */
+const ES_AR: StaticTextDefaultValue = {
+  toolbarTips: {
+    bold: 'negrita',
+    underline: 'subrayado',
+    italic: 'italica',
+    strikeThrough: 'tachado',
+    title: 'titulo',
+    sub: 'subindice',
+    sup: 'sobreindice',
+    quote: 'cita',
+    unorderedList: 'lista desordenada',
+    orderedList: 'lista ordenada',
+    task: 'lista de tareas',
+    codeRow: 'linea de código',
+    code: 'código a nivel de bloque',
+    link: 'link',
+    image: 'imagen',
+    table: 'tabla',
+    mermaid: 'sirena',
+    katex: 'formula',
+    revoke: 'deshacer',
+    next: 'rehacer',
+    save: 'guardar',
+    prettier: 'formatear',
+    pageFullscreen: 'pantalla completa en la página',
+    fullscreen: 'pantalla completa',
+    preview: 'vista previa',
+    htmlPreview: 'vista previa html',
+    catalog: 'catalogo',
+    github: 'código fuente',
+  },
+  titleItem: {
+    h1: 'Título H1',
+    h2: 'Título H2',
+    h3: 'Título H3',
+    h4: 'Título H4',
+    h5: 'Título H5',
+    h6: 'Título H6'
+  },
+  imgTitleItem: {
+    link: 'agregar enlace de imagen',
+    upload: 'cargar imagen',
+    clip2upload: 'subir video'
+  },
+  linkModalTips: {
+    descLabel: 'Desc:',
+    descLabelPlaceHolder: 'Introduzca una descripción...',
+    urlLabel: 'Link:',
+    urlLabelPlaceHolder: 'Introduzca link...',
+    buttonOK: 'OK'
+  },
+  clipModalTips: {
+    title: 'delimitar imagen',
+    buttonUpload: 'subir'
+  },
+  copyCode: {
+    text: 'Copiar',
+    successTips: 'Copiado!',
+    failTips: 'Error al copiar!'
+  },
+  mermaid: {
+    flow: 'flujo',
+    sequence: 'secuencia',
+    gantt: 'gráfico de gantt',
+    class: 'clases',
+    state: 'estado',
+    pie: 'pastel',
+    relationship: 'relación',
+    journey: 'ruta'
+  },
+  katex: {
+    inline: 'linea',
+    block: 'bloque',
+  },
+  footer: {
+    markdownTotal: 'Cantidad de caracteres',
+    scrollAuto: 'Auto Scroll'
+  }
+};
+
+export default ES_AR;
